08bc99696a8ea7bebfae8a2917b48bc9b14925d0
chyccs <chyccs@naver.com>
2023-04-20 18:21:01 +0900

    build: fix actions (#6)

--- SentryDotnetDiagnostics/Properties/AssemblyInfo.cs
+++ SentryDotnetDiagnostics/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("SentryDotnetDiagnostics")]
+[assembly: AssemblyTitle("SentryDiagnostics for .Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SentryDotnetDiagnostics")]
+[assembly: AssemblyProduct("SentryDiagnostics for .Net")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
