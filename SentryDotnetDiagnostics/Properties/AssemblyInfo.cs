5be8bea67f57864c514f2c18efd9551b60fa25ad
chyccs <chyccs@naver.com>
2023-04-20 20:18:03 +0900

    build: fix actions to push into nuget gallery (#9)

--- SentryDotnetDiagnostics/Properties/AssemblyInfo.cs
+++ SentryDotnetDiagnostics/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("SentryDiagnostics for .Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("DiagnosticsExtensions.Sentry")]
+[assembly: AssemblyDescription("Sentry Diagnostics Extensions for .NET framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SentryDiagnostics for .Net")]
+[assembly: AssemblyCompany("choi")]
+[assembly: AssemblyProduct("DiagnosticsExtensions.Sentry")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호를
 // 기본값으로 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.2")]
+[assembly: AssemblyFileVersion("0.0.1.2")]
